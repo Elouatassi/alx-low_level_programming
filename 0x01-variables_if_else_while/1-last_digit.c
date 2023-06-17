1ae0c69d2ef448ee5fa949a13cb88e4688a85f7b
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-16 19:05:48 -0700

    C Language tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (digit ==0)
+	if (digit == 0)
 		printf("Last digit of %i is %i and is 0\n", n, digit);
 	else if (digit < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
