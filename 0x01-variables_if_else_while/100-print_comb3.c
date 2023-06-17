6f0f54d59c7a99275827d964b0f5c9eb2e04abd4
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-17 16:44:51 -0700

    C Language tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int A, B;
+
+	for (A = '0'; A < '9'; A++)
+	{
+
+	for (B = A + 1; A <= '9'; B++)
+	{
+	if (A != B)
+	{
+	putcahr(A);
+	Putchar(B);
+	if (A == '8' && B == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
