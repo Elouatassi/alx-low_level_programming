491fcff38210835203e35a3998a928e2264424b0
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-17 13:58:19 -0700

    C Language tasks

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char teb;
+
+	for (teb = 'z'; teb <= 'a'; teb--)
+	putchar(teb);
+	putchar('\n');
+	return (0);
+}
