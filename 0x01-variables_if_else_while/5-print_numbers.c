3310d876ba6faab7db7925552f5ce2de3b005d3d
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-17 12:51:22 -0700

    C Language tasks

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Prints single digit numbers
+ *
+ * Return: Always 0 (Successe)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d \n", n);
+	return (0);
+}
