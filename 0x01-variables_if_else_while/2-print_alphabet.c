79f68d86bd0192f0617d527a35bb942d5784f88d
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-16 19:28:42 -0700

    C Language tasks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,24 @@
-#includ <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Etry point
  *
- * Description: print all alphabet lettre*/
+ * Description: print all alphabet letters
+ *
+ * Retur: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
