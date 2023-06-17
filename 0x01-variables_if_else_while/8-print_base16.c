f55bf2dd3b6cab459b5859608df4f70339b3b653
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-17 14:08:35 -0700

    C Language tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints numbers of base 16
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	char a;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	for (a = 'a'; a <= 'z'; a++)
+	putchar(a);
+	putchar('\n')
+
+	return (0);
+}
