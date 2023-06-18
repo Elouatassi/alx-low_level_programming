bcc67cfe1cfce43d36633959503d6628fb576ef7
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-17 17:36:48 -0700

    C Language tasks

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+
+	for (b = a + 1; b <= '9'; b++)
+	{
+
+	for (c = b + 1; c <= '9'; c++)
+	{
+	if ((a != b) != c)
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
