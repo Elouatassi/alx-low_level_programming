b6972e7c5a4d8640c06b58f8e352fea68296cb22
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-20 16:00:47 -0700

    C Language tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,12 @@
 
 /**
  * print_alphabet - make alphabet in lowercase
- * Return: void
 */
 
 void print_alphabet(void)
 {
 	int c;
-
 	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
+		_putchar(c);
 	_putchar('\n');
 }
