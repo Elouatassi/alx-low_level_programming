92688aa8c3788802202372cd54a5c760a63be26e
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-20 18:43:45 -0700

    C Language tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: return 1 if n is postive -1 if n is negative or 0 for anythig else
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else if (n >0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else 
+	{
+	_putchar(48);
+	return (0);
+	}
+}
