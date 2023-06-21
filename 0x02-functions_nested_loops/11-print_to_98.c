799082c95335759a068a2e96937d7f2146e5dbd6
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-21 16:17:35 -0700

    C Language tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,33 @@
 
 /**
  * print_to_98 - prints all natural numbers from 98
- *
  * @n: input
+ * Return: Always 0 (Succuss)
 */
 
 void print_to_98(int n)
 {
 	int i;
-
+	
 	if (n > 98)
+	{
 		for (i = n; i > 98; i++)
-			printf("%d, ", i);
-	else
-		for (i = n; i < 98; i--)
-			printf("%d, ", i);
-	printf("98\n");
-
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			for (i = n; i < 98; i--)
+			{
+				_putchar(i);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar("98");
+	_Putchar("\n");
+	return (0)
+}
