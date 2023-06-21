b582aaad41f6df8c354415d2b7582a94882d6ff8
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-20 19:57:07 -0700

    C Language tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  add - adds two integers and returns the result
+ * @a: first number to be computed
+ * @b: second number to be copmuted
+ * Return: integer value
+*/
+
+int add(int a, intb)
+{
+
+	return (a + b);
+}
