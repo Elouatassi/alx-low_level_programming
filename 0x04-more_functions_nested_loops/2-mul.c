f8c327cd8aaa625925b6bb2d2e641695ae29fb58
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-22 13:53:09 -0700

    C Language tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number to multiply
+ * @b: second number to myltiply
+ * Return: result of multiply between a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
