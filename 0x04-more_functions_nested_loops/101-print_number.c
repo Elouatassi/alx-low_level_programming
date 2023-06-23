e47f8ea7b4fdb1350d2437ae5093cee8201c2c00
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-22 17:35:34 -0700

    C Language tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the to be printed
+*/
+
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		a = -a;
+	}
+	if ((a / 10) > 0)
+		print_number(a / 10);
+	_putchar((a % 10) + '0');
+}
