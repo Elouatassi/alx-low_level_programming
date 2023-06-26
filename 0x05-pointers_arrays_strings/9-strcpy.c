5ef22ce76409ca5789603fd1f92aa4fd5d592e72
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-26 13:15:22 -0700

    Array Tasks

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: destination value
+ * @src: source value
+ * Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (scr[i] != '\0');
+
+	return (dest);
+}
