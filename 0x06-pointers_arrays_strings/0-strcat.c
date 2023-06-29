af99e82d9fe4067420c542c6000d7f7833720991
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-29 16:14:12 -0700

    Arrays and pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: pointer to destination input
  * @src: pointer to source input
- * Return: pointer to resulting string des
+ * Return: pointer to resulting string dest
 */
 
 char *_strcat(char *dest, char *src)
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[a])
 		a++;
 
-	for (b = 0; crs[b]; b++)
+	for (b = 0; src[b]; b++)
 		dest[a++] = src[b];
 
 	return (dest);
