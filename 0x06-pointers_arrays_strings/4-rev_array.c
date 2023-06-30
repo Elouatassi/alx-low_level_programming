6c9552afd9bed779e8bb3f6bc6053a286222f575
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-29 17:39:26 -0700

    Arrays and pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers to be reversed
+ * @n: nmber of element in the array
+ * Return: nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+	int x, t;
+
+	for (x = n - 1; x >= n / 2; x--)
+	{
+		t = a[n - 1 - x];
+		a[n - 1 - x] = a[x];
+		a[x] = t;
+	}
+}
