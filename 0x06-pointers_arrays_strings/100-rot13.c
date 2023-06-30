7f63d736661f64ad2f95499017fa2437f8f5b1b1
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-29 19:17:26 -0700

    Arrays and pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - encodes a string using rot13
@@ -11,7 +12,6 @@ char *rot13(char *c)
 	int i, j;
 	char donn[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char donnrot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
