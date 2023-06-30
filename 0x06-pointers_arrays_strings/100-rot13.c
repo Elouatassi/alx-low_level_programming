9f4e620323d3682fd7bc3c9dbc142b7b272b3118
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-06-29 19:11:37 -0700

    Arrays and pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @c: input string
+ * Return: encoded string
+*/
+
+char *rot13(char *c)
+{
+	int i, j;
+	char donn[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char donnrot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == donn[j])
+			{
+				c[1] = donnrot13[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
