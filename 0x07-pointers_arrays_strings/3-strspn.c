ba910bfe56c66cfb8e77394000659b55bbf9c9da
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-03 17:17:27 -0700

    pointer and arrays

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,19 +6,16 @@
  * @accept: bytes
  * Return: unsigned int
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != NULL; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (accept[j] == NULL)
-			{
+			if (accept[j] == '\0')
 				return (i);
-			}
 		}
 	}
 	return (i);
