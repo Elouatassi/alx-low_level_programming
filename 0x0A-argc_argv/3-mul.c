e6fc8f0ff1f2aae4c55958e74e6dd2c3b1767576
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-07 17:41:20 -0700

    ArgC and ArgV tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int som;
+
+	if (argc == 3)
+	{
+		som = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", som);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
