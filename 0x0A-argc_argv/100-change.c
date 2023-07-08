ee2f5925a19ad41fd78723f7cdd7f9f1434a12f4
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-07 19:03:17 -0700

    ArgC and ArgV tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,32 +4,31 @@
 /**
  * main - prints the minimum number of coins
  * to make change for an amount of money
- *
  * @argc: Argument count
  * @argv: Argument vector
- * Return: 0-success or 1 if fail
+ * Return: 0-success non-zero-fail
 */
 
 int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int x, cents = 0, coins = atoi(argv[1]);
-		int cent[] = {25, 10, 5, 2, 1};
+	int x, cents = 0, coins = atoi(argv[1]);
+	int cent[] = {25, 10, 5, 2, 1};
 
-		for (x = 1; x < 5; x++)
+	for (x = 1; x < 5; x++)
+	{
+		if (coins >= cent[x])
 		{
-			if (coins >= cent[x])
+			cents += coins / cent[x];
+			coins = coins % cent[x];
+			if (coins % cent[x] == 0)
 			{
-				cents += coins / cent[x];
-				coins = coins % cent[x];
-				if (coins % cent[x] == 0)
-				{
-					break;
-				}
+				break;
 			}
 		}
-		printf("%d\n", cents);
+	}
+	printf("%d\n", cents);
 	}
 	else
 	{
