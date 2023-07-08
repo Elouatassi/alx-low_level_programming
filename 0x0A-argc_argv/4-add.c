277e85ca35ec33887d241fc84486571bd00608dc
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-07 18:08:10 -0700

    ArgC and ArgV tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int som = 0;
+	char *x;
+
+	while (--argc)
+	{
+		for (x = argv[argc]; *x; x++)
+		{
+			if (*x < 48 || *x > 57)
+			{
+				return (printf("Erroe\n"), 1);
+			}
+			som += atoi(argv[argc]);
+		}
+	}
+	printf("%d\n", som);
+	return (0);
+}
