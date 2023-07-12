a37fa5a9da0a2d573606a6b076c425ce9ae7e3e5
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-12 16:15:05 -0700

    Malloc and Free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -37,7 +37,7 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	n = wrdctn(str);
+	n = wrdcnt(str);
 	if (n == 1)
 		return (NULL);
 	y = (char **)malloc(n * sizeof(char *));
