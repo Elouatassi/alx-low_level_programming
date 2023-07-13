c70293cec125f9cdd03d0385d7ddba06144d07e4
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-13 15:53:24 -0700

    More Malloc and Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		l = i + n;
 
-	str = maloc(sizeof(char) * l + 1);
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 
