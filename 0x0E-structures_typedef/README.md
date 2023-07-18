c8d6b480df6c8f63a076e5ae8f5b5e4ee42e8da4
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-17 17:23:20 -0700

    C Language tasks

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Read Structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOH_H
+#define DOH_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog' name
+ * @age: dog's Age
+ * @owner: owner's name
+ * Description: A long dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif
