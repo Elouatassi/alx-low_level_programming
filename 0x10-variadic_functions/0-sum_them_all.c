f3c7bf04b05438f71649c8d363a094d4739ad675
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-20 16:36:36 -0700

    C Language tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_function.h"
+
+/**
+ * sum_them_all - sums variable arguments
+ * @n: Number of argument
+ * Return: Integer sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int som = 0, i;
+	va_list argl;
+
+	if (n == 0)
+		return (0);
+
+	va_start (argl, n);
+
+	for (i = 0; i < n; i++)
+	{
+		som += va_arg(argl, int);
+	}
+	va_end(argl);
+
+	return (som);
+}
