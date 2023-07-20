52de35ceff76fd1951abf7f8854e4ea3aef89577
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-19 18:51:17 -0700

    C Language tasks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - chek the code
+ * @argc: Number of args
+ * @argv: argument vector
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int x;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	x = atoi(argv[1]);
+	if (x < 0)
+		printf("Error\n"), exit(2);
+
+	while (x--)
+		printf("%02hhx%s", *p++, x ? " " : "\n");
+	return (0);
+}
