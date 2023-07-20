9bc370d9d08a1e7ac8161ddb23372c93d3c3757f
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-19 17:27:16 -0700

    C Language tasks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
  * Return: Nothing
 */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
