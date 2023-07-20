e6f75d03207c16aeb99af12da88adcbf50f30557
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-19 18:37:01 -0700

    C Language tasks

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ * @argc: Number of args
+ * @argv: Argument vector
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Product
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Quotient
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo two integers
+ * @a: First integer
+ * @b: second integer
+ * Return: Remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
