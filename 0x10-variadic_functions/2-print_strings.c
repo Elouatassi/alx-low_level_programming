7f72d1d06723b9b1fbf816442a4f01af9c9e9f42
EL OUATASSI Marouane <elouatassimarouane@gmail.com>
2023-07-20 18:00:23 -0700

    C Language tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (str == NULL)
-			str = "nil";
+			str = "(nil)";
 		printf("%s", str);
 		if (i < n - 1)
 		{
