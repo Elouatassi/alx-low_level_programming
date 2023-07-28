ae008271e7240de5904e20d7e043473848766a69
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-27 17:53:55 -0700

    Tasks

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "liste.h"
 
 /**
- * liste_len - returns the number of elements in a linked list_t list
+ * list_len - determines length of linked list
  * @h: Pointer
  * Return: Size of list
 */
