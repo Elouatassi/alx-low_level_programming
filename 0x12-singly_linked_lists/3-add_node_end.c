9a468d88e907ab319d0392f23ffd27a6f13bfeef
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-07-27 18:35:55 -0700

    Tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,15 +2,15 @@
 
 /**
  * add_node_end - adds a node to the end of a list
- * @head: Addresse of pointer to head node
- * @str: String field of node
- * Return: SIze of list
+ * @head: Adresse of pointer to head node
+ * @str: str field of node
+ * Return: Size of list
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n = malloc(sizeof(list_t));
-	liste_t *node = *head;
+	list_t *node = *head;
 
 	if (!head || !n)
 		return (NULL);
