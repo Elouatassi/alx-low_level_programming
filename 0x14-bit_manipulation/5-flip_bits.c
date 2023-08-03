8b159ee078cbaa524b99032f867f92af489a1fb6
Marouane ELOUATASSI <elouatassimarouane@gmail.com>
2023-08-03 15:48:51 -0700

    tasks

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int flip_bits - returns the number of bits you would need to flip
+ * @n: 1st Number
+ * @m: 2nd Number
+ * Return: Number
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int c = 0;
+
+	while (x)
+	{
+		if (x & 1ul)
+			c++;
+		x = x >> 1;
+	}
+	return (c);
+}
